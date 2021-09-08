65a2e2730eb26c1877faf77849dc088b738a992d
today8934 <today8934@gmail.com>
2021-09-08 21:19:52 +0900

    build image

---
+++ web/app.js
@@ -0,0 +1,19 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'docker' })
+})
+
+// Run the server!
+fastify.listen(3000, '0.0.0.0', function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  fastify.log.info('server listening on ${address}')
+  // Server is now listening on ${address}
+})
